ed981f960dd3f5de1f2e864d49e930f94512cb6b
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-24 21:52:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,11 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  number = 10 
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
