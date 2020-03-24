6978d2bfbb76defc93aec656f7e9f13d570deea4
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-24 21:50:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
-number = 10 
-while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1 
+def countdown(number)
+  number = 10 
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
 end
-
 return "HAPPY NEW YEAR!"
